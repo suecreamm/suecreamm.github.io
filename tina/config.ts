9c7eb6bd638cda73ff7d8d8082c108898d01bc10
suecream <100839777+suecreamm@users.noreply.github.com>
2024-03-17 18:09:59 +0900

    Update config.ts

--- tina/config.ts
+++ tina/config.ts
@@ -10,8 +10,8 @@ const branch =
 export default defineConfig({
   branch,
 
-  clientId: process.env['NEXT_PUBLIC_TINA_CLIENT_ID'],
-  token: process.env['TINA_TOKEN'],
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  token: process.env.TINA_TOKEN,
 
   build: {
     outputFolder: "admin",

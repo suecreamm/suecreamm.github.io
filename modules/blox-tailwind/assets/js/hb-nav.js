3280f4f8992f207c3146c8b9ed26f10da5f01170
suecreamm <100839777+suecreamm@users.noreply.github.com>
2025-04-17 20:47:45 +0900

    Add files via upload

---
+++ modules/blox-tailwind/assets/js/hb-nav.js
@@ -0,0 +1,23 @@
+// Navigation
+
+const applyScrollPadding = () => {
+  const header = document.querySelector('.page-header');
+  let position = header.getBoundingClientRect();
+  document.documentElement.style.scrollPaddingTop = position.height.toString() + 'px';
+  const r = document.querySelector(':root');
+  r.style.setProperty('--navbar-height', position.height.toString() + 'px');
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  const dropdownMenus = document.querySelectorAll(
+    ".nav-dropdown > .nav-link",
+  );
+
+  dropdownMenus.forEach((toggler) => {
+    toggler?.addEventListener("click", (e) => {
+      e.target.parentElement.classList.toggle("active");
+    });
+  });
+
+  applyScrollPadding()
+});
